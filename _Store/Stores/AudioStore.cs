de4012efe2ed097bf4ca9839a150ae9ab0441396
max long <long.maxime@gmail.com>
2017-04-13 11:39:55 -0700

    store and views

---
+++ _Store/Stores/AudioStore.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStore : MonoBehaviour {
+
+
+	//start observable pattern
+	public U3D.KVO.ReadOnlyValueObserving<float> echoAmount { get { return s_echoAmount; } } 
+	U3D.KVO.ValueObserving<float> s_echoAmount = new U3D.KVO.ValueObserving<float>();
+
+	public U3D.KVO.ReadOnlyValueObserving<float> chorusAmount { get { return s_chorusAmount; } } 
+	U3D.KVO.ValueObserving<float> s_chorusAmount = new U3D.KVO.ValueObserving<float>();
+
+	public U3D.KVO.ReadOnlyValueObserving<float> reverbAmount { get { return s_reverbAmount; } } 
+	U3D.KVO.ValueObserving<float> s_reverbAmount = new U3D.KVO.ValueObserving<float>();
+
+	public U3D.KVO.ReadOnlyValueObserving<bool> looper { get { return s_looper; } } 
+	U3D.KVO.ValueObserving<bool> s_looper = new U3D.KVO.ValueObserving<bool>();
+
+	public U3D.KVO.ReadOnlyValueObserving<string> sample { get { return s_sample; } } 
+	U3D.KVO.ValueObserving<string> s_sample = new U3D.KVO.ValueObserving<string>();
+
+	public U3D.KVO.ReadOnlyValueObserving<string> scale { get { return s_scale; } } 
+	U3D.KVO.ValueObserving<string> s_scale = new U3D.KVO.ValueObserving<string>();
+
+
+	// Initializing values
+	void Start () {
+		s_echoAmount.set = 0f;
+		s_chorusAmount.set = 0f;
+		s_reverbAmount.set = 0f;
+		s_looper.set = false;
+
+		s_sample.set = "fuzz";
+		s_scale.set = "major";
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ _Store/Views/ScaleView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleView : MonoBehaviour {
+
+	private AudioStore store;
+	private Text scaleTracker;
+
+	// Use this for initialization
+	void Start () {
+		store = GameObject.FindGameObjectWithTag ("Store_Audio").GetComponent<AudioStore>();
+		store.scale.RegisterObserver (updateScale);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void updateScale(string scale){
+		scaleTracker = transform.parent.FindChild ("Scale").GetComponent<Text>();
+		scaleTracker.text = scale;
+	}
+}
